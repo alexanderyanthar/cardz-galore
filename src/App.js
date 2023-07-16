cc9a813203f350cd2f337367c6ac8d02b8e3c485
alexanderyanthar <alexander.yanthar@outlook.com>
2023-07-16 14:50:18 -0400

    made basic layout for navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='flex justify-between items-center w-11/12 max-w-screen-xl mx-auto my-0 h-24'>
+      <div>
+        <h1 className='text-2xl'>Cardz Galore</h1>
+      </div>
+      <div className='flex'>
+        <ul className='flex justify-between items-center'>
+        <li className='px-2'><a href="#home">Home</a></li>
+        <li className='px-2'><a href="#buy-cards">Buy Cards</a></li>
+        <li className='px-2'><a href="#sell-cards">Sell Cards</a></li>
+        <li className='px-2'><a href="#explore">Explore</a></li>
+      </ul>
+        <button className='ml-4 bg-orange-600 px-3 py-2 rounded'>Sign In</button>
+      </div>
     </div>
   );
 }
